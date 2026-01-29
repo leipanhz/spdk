e3b04928338b46d869e12bdc4f7c22f48322ab7c
Lei Pan <leipan@ibm.com>
2026-01-29 18:45:18 -0500

    Update AIO creation script (working)

--- lps_16ssdBuildAio.sh
+++ lps_16ssdBuildAio.sh
@@ -3,6 +3,12 @@
 
 set -x
 
+# Ensure kernel target is OFF
+sudo modprobe -r nvmet_rdma nvmet
+
+# reset
+sudo ./scripts/setup.sh reset
+
 NQN="nqn.2025-12.arc.spdk:wolf20"
 SN="SPDK202512ARCWOFL20"
 
@@ -15,21 +21,25 @@ sudo scripts/rpc.py iobuf_set_options \
   --large-pool-count 8192
 sudo scripts/rpc.py framework_start_init
 
-sleep 5; 
+sleep 5;
 read -p "Press Enter to continue  \n" </dev/tty
 
+# Create 16 aio block devices
+for i in {0..15}; do
+  # sudo ./scripts/rpc.py bdev_aio_create /dev/nvme${i}n1 aio${i} 4096; sleep 2;
+  sudo ./scripts/rpc.py bdev_aio_create /dev/nvme${i}n1 aio${i} 512;
+done
 
-for i in {0..15}; do sudo ./scripts/rpc.py bdev_aio_create /dev/nvme${i}n1 aio${i} 4096; sleep 2; done
-
+# Configure NVMe-oF Transport with RDMA
 sudo scripts/rpc.py nvmf_create_transport -t RDMA -u 8192 -i 131072 -c 8192
 sudo scripts/rpc.py nvmf_get_transports
 
+# Create subsystem
 sudo scripts/rpc.py nvmf_create_subsystem $NQN -a -s $SN
 sudo scripts/rpc.py nvmf_get_subsystems
 
-
-bdevs=$(sudo scripts/rpc.py bdev_get_bdevs | grep \"name\" | cut -d"\"" -f4)
 # Add namespace
+bdevs=$(sudo scripts/rpc.py bdev_get_bdevs | grep \"name\" | cut -d"\"" -f4)
 for ns in $bdevs; do \
 	sudo scripts/rpc.py nvmf_subsystem_add_ns $NQN $ns; \
 done
@@ -39,8 +49,8 @@ sudo scripts/rpc.py nvmf_get_subsystems | less
 sudo scripts/rpc.py bdev_nvme_get_controllers | less
 
 read -p "Press Enter to add listener" </dev/tty
-#sudo scripts/rpc.py nvmf_subsystem_add_listener  $NQN -t rdma -a 10.243.3.20 -s 4420
-sudo scripts/rpc.py nvmf_subsystem_add_listener  $NQN -t rdma -a 10.243.3.20 -s 4421
+sudo scripts/rpc.py nvmf_subsystem_add_listener  $NQN -t rdma -a 10.243.3.20 -s 4420
+# sudo scripts/rpc.py nvmf_subsystem_add_listener  $NQN -t rdma -a 10.243.3.20 -s 4421
 
 read -p "Press Enter to save config, otherwise ctrl+C to exit" </dev/tty
 sudo scripts/rpc.py save_config > ~/Config/spdk/spdk_config_0123_16ssd_aio.json
